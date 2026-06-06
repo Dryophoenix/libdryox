7a0e9355868c5b83f05a5fd77ab612bff12146df
Neph Hillis <dryo@dryophoenix.net>
2026-06-06 16:27:03 -0700

    test: dryoinit test refactored.

--- src/tests/dryoinit.c
+++ src/tests/dryoinit.c
@@ -1,5 +1,7 @@
 #include "tests/dryoinit.h"
 #include "dryox/dryoinit.h"
+#include "tests/ENVUTILS.h"
+#include "tests/REPORT.h"
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,55 +17,107 @@
 
 void TEST_dryoinit(void)
 {
-  int result;
-  char teststr[PATH_MAX];
+  char *overridebuf;
 
-  result = dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE);
-  if (result == 0)
-    printf("dryoinit passed,\n");
-  else
-    printf("dryoinit failed,\n");
-
-  setenv("DENV_OVERRIDE_STATE", "tests/init/OVERRIDE/state", 1);
-  result = dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE);
-  if (result == 0)
-    printf("dryoinit OVERRIDE passed,\n");
-  else
-    printf("dryoinit OVERRIDE failed,\n");
-  unsetenv("DENV_OVERRIDE_STATE");
-
-  setenv("XDG_STATE_HOME", "tests/init/XDG/state", 1);
-  result = dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE);
-  if (result == 0)
-    printf("dryoinit XDG passed,\n");
-  else
-    printf("dryoinit XDG failed,\n");
-  unsetenv("XDG_STATE_HOME");
-
-  char HOME_BUF[PATH_MAX];
-  const char *HOME_ORIG = getenv("HOME");
-  if (HOME_ORIG != NULL)
-    snprintf(HOME_BUF, PATH_MAX, "%s", HOME_ORIG);
-
-  setenv("HOME", "tests/init/HOME/state", 1);
-  result = dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE);
-  if (result == 0)
-    printf("dryoinit HOME passed,\n");
-  else
-    printf("dryoinit HOME failed,\n");
-
-  if (HOME_ORIG != NULL)
-    setenv("HOME", HOME_BUF, 1);
-  else
-    unsetenv("HOME");
-
-  char LOG_FILE[PATH_MAX];
-  result = dryoinit(LOG_FILE, NULL, NULL, DRYOX_LITERAL, "tests/logging/test.log");
-  if (result == 0)
-    printf("dryoinit (DRYOX_LITERAL) passed,\n");
-  else
+  // STATE
   {
-    snprintf(LOG_FILE, PATH_MAX, "tests/logging/test.log");
-    printf("dryoinit (DRYOX_LITERAL) failed,\n");
+    char teststr[PATH_MAX];
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE), "state");
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("DENV_OVERRIDE_STATE", overridebuf, "tests/init/OVERRIDE/state");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE), "state OVERRIDE");
+    RESTOREENV("DENV_OVERRIDE_STATE", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("XDG_STATE_HOME", overridebuf, "tests/init/XDG/state");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE), "state XDG");
+    RESTOREENV("XDG_STATE_HOME", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("HOME", overridebuf, "tests/init/HOME/state");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_STATE), "state HOME");
+    RESTOREENV("HOME", overridebuf);
+  }
+
+  // CONFIG
+  {
+    char teststr[PATH_MAX];
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CONFIG), "config");
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("DENV_OVERRIDE_CONFIG", overridebuf, "tests/init/OVERRIDE/config");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CONFIG), "config OVERRIDE");
+    RESTOREENV("DENV_OVERRIDE_CONFIG", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("XDG_CONFIG_HOME", overridebuf, "tests/init/XDG/config");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CONFIG), "config XDG");
+    RESTOREENV("XDG_CONFIG_HOME", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("HOME", overridebuf, "tests/init/HOME/config");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CONFIG), "config HOME");
+    RESTOREENV("HOME", overridebuf);
+  }
+
+  // DATA
+  {
+    char teststr[PATH_MAX];
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_DATA), "data");
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("DENV_OVERRIDE_DATA", overridebuf, "tests/init/OVERRIDE/data");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_DATA), "data OVERRIDE");
+    RESTOREENV("DENV_OVERRIDE_DATA", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("XDG_DATA_HOME", overridebuf, "tests/init/XDG/data");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_DATA), "data XDG");
+    RESTOREENV("XDG_DATA_HOME", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("HOME", overridebuf, "tests/init/HOME/data");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_DATA), "data HOME");
+    RESTOREENV("HOME", overridebuf);
+  }
+
+  // CACHE
+  {
+    char teststr[PATH_MAX];
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CACHE), "cache");
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("DENV_OVERRIDE_CACHE", overridebuf, "tests/init/OVERRIDE/cache");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CACHE), "cache OVERRIDE");
+    RESTOREENV("DENV_OVERRIDE_CACHE", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("XDG_CACHE_HOME", overridebuf, "tests/init/XDG/cache");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CACHE), "cache XDG");
+    RESTOREENV("XDG_CACHE_HOME", overridebuf);
+  }
+  {
+    char teststr[PATH_MAX];
+    CACHESETENV("HOME", overridebuf, "tests/init/HOME/cache");
+    REPORT(dryoinit(teststr, "test.log", "TestProject", DRYOX_XDG_CACHE), "cache HOME");
+    RESTOREENV("HOME", overridebuf);
+  }
+
+  // LITERAL
+  {
+    char teststr[PATH_MAX];
+    REPORT(dryoinit(teststr, NULL, NULL, DRYOX_LITERAL, "tests/logging/test.log"), "literal");
   }
 }

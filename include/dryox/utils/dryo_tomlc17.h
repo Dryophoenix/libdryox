282102c7f566740285ebdf200155db46893b773f
Neph Hillis <dryo@dryophoenix.net>
2026-06-06 23:39:17 -0700

    test: removed vestigial 'overridebuf' from dryoinit test

---
+++ include/dryox/utils/dryo_tomlc17.h
@@ -0,0 +1,4 @@
+#ifndef DRYOU_TOML_H
+#define DRYOU_TOML_H
+
+#endif
---
+++ src/utils/dryo_tomlc17.c
@@ -0,0 +1,21 @@
+#include "utils/dryo_tomlc17.h"
+#include "dryox/dryologging.h"
+#include "tomlc17/tomlc17.h"
+
+/*
+ * This is essentially just a wrapper of tomlc17 for a stable API.
+ *
+ * As though tomlc17 is unstable.
+ */
+
+toml_result_t dryo_toml_parse(FILE *f)
+{
+  toml_result_t result = toml_parse_file(f);
+  if (result.ok != true)
+  {
+    dryolog(LOG_ERROR, "");
+  }
+  return result;
+}
+
+void dryo_toml_free(toml_result_t result) { toml_free(result); }
